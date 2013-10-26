b3c7def087f60dca7d161385254ba7fb51f6fa39
Brett Delle Grazie <brett.dellegrazie@gmail.com>
2013-10-26 19:50:36 +0100

    Put powerline font config in with powerline

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 !/bin/bash
 
-sudo apt-get install stow git libgit2-dev
+sudo apt-get install python-pip python-psutils stow git libgit2-dev
 pip install --user git+git://github.com/Lokaltog/powerline
 pip install --user pygit2
 
-stow -t ~ bash fonts tmux vim
+stow -t ~ bash powerline tmux vim
 fc-cache -vf ~/.fonts
 
