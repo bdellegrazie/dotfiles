cedae4015bdd623a095c9ea8b9070fd04376da52
Brett Delle Grazie <brett.dellegrazie@gmail.com>
2013-12-21 12:45:14 +0000

    Agree to install

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-sudo apt-get install python-dev python-pip python-psutil stow git libgit2-dev
+sudo apt-get install -y python-dev python-pip python-psutil stow git libgit2-dev
 pip install --user git+git://github.com/Lokaltog/powerline
 LIBGIT2VER=$(dpkg-query --showformat='${source:Version}\n' --show libgit2-0)
 LIBGIT2VER=${LIBGIT2VER%-*}
