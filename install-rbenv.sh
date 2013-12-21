84909009b8c9fe1dbf898b332b71ac4f104d34fe
Brett Delle Grazie <brett.dellegrazie@gmail.com>
2013-12-21 12:45:31 +0000

    Initial installer for rbenv and plugins

---
+++ install-rbenv.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+sudo apt-get install -y build-essential autoconf git libssl-dev libyaml-dev libreadline6 libreadline6-dev zlib1g zlib1g-dev
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+PLUGIN_DIR=~/.rbenv/plugins
+mkdir -p $PLUGIN_DIR
+pushd $PLUGIN_DIR >/dev/null
+git clone https://github.com/sstephenson/ruby-build.git
+git clone https://github.com/sstephenson/rbenv-vars.git
+git clone https://github.com/sstephenson/rbenv-gem-rehash.git
+git clone https://github.com/sstephenson/rbenv-default-gems.git
+git clone https://github.com/tpope/rbenv-communal-gems.git
+git clone https://github.com/chriseppstein/rbenv-each.git
+git clone https://github.com/jf/rbenv-gemset.git
+git clone https://github.com/rkh/rbenv-update.git
+git clone https://github.com/rkh/rbenv-use.git
+git clone https://github.com/rkh/rbenv-whatis.git
+git clone https://github.com/tpope/rbenv-aliases.git
+git clone https://github.com/ianheggie/rbenv-binstubs.git
+popd >/dev/null
