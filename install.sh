6446a880bfa3f68e0db87fe49c53436f4b3aedbb
Brett Delle Grazie <brett.dellegrazie@gmail.com>
2013-12-24 10:34:36 +0000

    Pre-install vundle and powerline before running vim install, use the same URL pattern (https://github....)

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 sudo apt-get install -y python-dev python-pip python-psutil stow git libgit2-dev vim-nox
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+pip install --user --editable git+https://github.com/Lokaltog/powerline.git#egg=powerline --source ~/.vim/bundle
 vim +BundleInstall +qall
-pip install --user --editable git+git://github.com/Lokaltog/powerline#egg=powerline --source ~/.vim/bundle
 LIBGIT2VER=$(dpkg-query --showformat='${source:Version}\n' --show libgit2-0)
 LIBGIT2VER=${LIBGIT2VER%-*}
 pip install --user pygit2==$LIBGIT2VER
