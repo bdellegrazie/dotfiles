0a4175a19982dbdf94dbd9b37fb15c73f4126b19
Brett Delle Grazie <brett.dellegrazie@indigoblue.co.uk>
2014-01-13 15:13:33 +0000

    Use symbolic links for font configuration to keep up-to-date with Powerline

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash -x
-sudo apt-get install -y python-dev python-pip python-psutil stow git libgit2-dev vim-nox
+sudo apt-get install -y python-dev python-pip python-psutil stow git vim-nox
+sudo apt-get install -y libgit2-dev
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 pip install --user --editable git+https://github.com/Lokaltog/powerline.git#egg=powerline --source ~/.vim/bundle
 vim +BundleInstall +qall
 LIBGIT2VER=$(dpkg-query --showformat='${source:Version}\n' --show libgit2-0)
 LIBGIT2VER=${LIBGIT2VER%-*}
 pip install --user pygit2==$LIBGIT2VER
-
+mkdir -p ~/.fonts ~/.fonts.conf.d ~/.config/fontconfig/conf.d
+ln -sf ~/.vim/bundle/powerline/fonts/PowerlineSymbols.otf ~/.fonts/
+ln -sf ~/.vim/bundle/powerline/fonts/10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+ln -sf ~/.vim/bundle/powerline/fonts/10-powerline-symbols.conf ~/.fonts.conf.d/
 stow -t ~ bash powerline tmux vim
 fc-cache -vf ~/.fonts
 
