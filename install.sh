08ca8d5c7ef5fcffe980362b65dfba17d92dab4e
Brett Delle Grazie <brett.dellegrazie@gmail.com>
2014-01-14 07:57:58 +0000

    path is 'font' not 'fonts' in powerline

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ LIBGIT2VER=$(dpkg-query --showformat='${source:Version}\n' --show libgit2-0)
 LIBGIT2VER=${LIBGIT2VER%-*}
 pip install --user pygit2==$LIBGIT2VER
 mkdir -p ~/.fonts ~/.fonts.conf.d ~/.config/fontconfig/conf.d
-ln -sf ~/.vim/bundle/powerline/fonts/PowerlineSymbols.otf ~/.fonts/
-ln -sf ~/.vim/bundle/powerline/fonts/10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
-ln -sf ~/.vim/bundle/powerline/fonts/10-powerline-symbols.conf ~/.fonts.conf.d/
+ln -sf ~/.vim/bundle/powerline/font/PowerlineSymbols.otf ~/.fonts/
+ln -sf ~/.vim/bundle/powerline/font/10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+ln -sf ~/.vim/bundle/powerline/font/10-powerline-symbols.conf ~/.fonts.conf.d/
 stow -t ~ bash powerline tmux vim
 fc-cache -vf ~/.fonts
 
