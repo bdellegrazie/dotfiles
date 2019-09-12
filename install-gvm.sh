c910030267ca91dc1a0874eeb528c2dc7c6301d9
Brett Delle Grazie <brett.dellegrazie@gmail.com>
2019-09-12 21:02:56 +0100

    No longer use gvm / rvm

--- install-gvm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-sudo apt-get install -y binutils bison curl gcc git mercurial make
-git clone https://github.com/moovweb/gvm.git ~/.gvm
-
-cat > $HOME/.gvm/scripts/gvm <END_GVM
-export GVM_ROOT=$HOME/.gvm
-. \$GVM_ROOT/scripts/gvm-default
-END_GVM
-
