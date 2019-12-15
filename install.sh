aa80261b12177a11df2e6ce630e870751dc073ad
Brett Delle Grazie <brett.dellegrazie@weareadaptive.com>
2019-12-15 21:33:29 +0000

    Switch to Python3 only, add direnv

--- install.sh
+++ install.sh
@@ -4,17 +4,11 @@ sudo apt-get install -y \
  fonts-powerline \
  git \
  powerline \
- python-dev \
  python3-dev \
- python-pip \
  python3-pip \
- python-powerline \
  python3-powerline \
- python-psutil \
  python3-psutil \
- python-pygit2 \
  python3-pygit2 \
- python-virtualenv \
  python3-venv \
  stow \
  vim-nox \
